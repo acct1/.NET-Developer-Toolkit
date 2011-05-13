5092c03b25a4c89bd27877173aad91636bdcbf71
Jeff Donios <jeff.donios@simplecontent.com>
2011-05-12 23:52:51 -0400

    Donios.DeveloperToolkit.Web + Tests

---
+++ Donios.DeveloperToolkit.Web.Tests/HttpUtilityTest.cs
@@ -0,0 +1,103 @@
+﻿namespace Donios.DeveloperToolkit.Web.Tests
+{
+    using Donios.DeveloperToolkit.Web;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;    
+    
+    /// <summary>
+    ///This is a test class for HttpUtilityTest and is intended
+    ///to contain all HttpUtilityTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class HttpUtilityTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for HttpUtility Constructor
+        ///</summary>
+        [TestMethod()]
+        public void HttpUtilityConstructorTest()
+        {
+            HttpUtility target = new HttpUtility();
+            Assert.IsNotNull(target);
+        }
+
+        /// <summary>
+        ///A test for HttpGet
+        ///</summary>
+        [TestMethod()]
+        public void HttpGetTest()
+        {
+            string URI = "http://www.google.com/#q=github";
+            string expected = "<title>Google</title>";
+            string actual;
+            actual = HttpUtility.HttpGet(URI, null);
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.Contains(expected));
+        }
+
+        /// <summary>
+        ///A test for HttpPost
+        ///</summary>
+        [TestMethod()]
+        public void HttpPostTest()
+        {
+            string URI = "http://posttestserver.com/post.php";
+            string expected = "dumped 2 post variables";
+            string actual;
+            actual = HttpUtility.HttpPost(URI, "q=github&q2=donios", null);
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.Contains(expected));
+        }
+    }
+}
---
+++ Donios.DeveloperToolkit.Web/HttpUtility.cs
@@ -0,0 +1,52 @@
+﻿namespace Donios.DeveloperToolkit.Web
+{
+    using System;
+    using System.Net;
+    using System.IO;
+
+    /// <summary>Utility classes for common HTTP operations</summary>
+    public class HttpUtility
+    {
+        //TODO: Cleanup
+        /// <summary>HTTP GET a URI and return a response</summary>
+        /// <param name="URI">URI to GET</param>
+        /// <param name="proxy">Set to null if a proxy will not be used</param>
+        /// <returns>Body of the response</returns>
+        /// <remarks>Based on code from http://www.hanselman.com/blog/HTTPPOSTsAndHTTPGETsWithWebClientAndCAndFakingAPostBack.aspx</remarks>
+        public static string HttpGet(string URI, WebProxy proxy)
+        {
+            WebRequest req = WebRequest.Create(URI);
+            if (proxy != null)
+                req.Proxy = proxy;
+            WebResponse resp = req.GetResponse();
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+        
+        //TODO: Cleanup
+        /// <summary>HTTP POST to a URI and return a response</summary>
+        /// <param name="URI">URI to POST parameters</param>
+        /// <param name="proxy">Set to null if a proxy will not be used</param>
+        /// <returns>Body of the response</returns>
+        /// <remarks>Based on code from http://www.hanselman.com/blog/HTTPPOSTsAndHTTPGETsWithWebClientAndCAndFakingAPostBack.aspx</remarks>
+        public static string HttpPost(string URI, string Parameters, WebProxy proxy)
+        {
+            WebRequest req = WebRequest.Create(URI);
+             if (proxy != null)
+                req.Proxy = proxy;
+            //Add these, as we're doing a POST
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Method = "POST";
+            //We need to count how many bytes we're sending. Post'ed Faked Forms should be name=value&
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(Parameters);
+            req.ContentLength = bytes.Length;
+            Stream os = req.GetRequestStream();
+            os.Write(bytes, 0, bytes.Length); //Push it out there
+            os.Close();
+            WebResponse resp = req.GetResponse();
+            if (resp == null) return null;
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+    }
+}
